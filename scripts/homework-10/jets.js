b05cad6dca756fe7232fad01291bb3b221557a85
Mir4ikqwe19 <amir.adilov.19@gmail.com>
2025-12-11 11:44:40 +0300

    [Homework №10]: complete import/export and classes tasks

---
+++ scripts/homework-10/jets.js
@@ -0,0 +1,31 @@
+class Jet {
+  constructor(company, model, engine, length, height) {
+    this.company = company;
+    this.model = model;
+    this.engine = engine;
+    this.length = length;
+    this.height = height;
+  };
+
+  getSizeInfo() {
+    return console.log(`Высота самолёта ${this.model}: ${this.height}, Длина самолёта ${this.model}:  ${this.length}`);
+  };
+};
+
+class FighterJet extends Jet {
+  constructor(company, model, engine, length, height, maxSpeed) {
+    super(company, model, engine, length, height);
+    this.maxSpeed = maxSpeed;
+  };
+
+  getEngineInfo() {
+    return console.log(`Двигатель истребителя ${this.model}:${this.engine}, Максимальная скорость истребителя ${this.model}:${this.maxSpeed}`);
+  };
+};
+
+const transportPlane = new Jet ('ОКБ ИЛЬЮШИНА', 'ИЛ-76', '4 х Д-30КП', '46,6 м', '14,76 м');
+const fighterPlane = new FighterJet ('ОКБ СУХОЙ', 'СУ-30', '2 х Сатурн АЛ-31', '21,9 м', '6,36 м', '2,120 км/ч');
+
+transportPlane.getSizeInfo();
+fighterPlane.getEngineInfo();
+fighterPlane.getSizeInfo();
